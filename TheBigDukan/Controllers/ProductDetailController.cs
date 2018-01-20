2b7092bc4d1700fef4869ce169c7be4d01ca3156
Hassan <hassan_farooqui90@yahoo.com>
2018-01-19 22:49:24 -0800

    work on details action show to the detail page

--- TheBigDukan/Controllers/ProductDetailController.cs
+++ TheBigDukan/Controllers/ProductDetailController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,11 +19,43 @@ namespace TheBigDukan.Controllers
             return View(db.Products.ToList());
         }
 
-
+        [HttpGet]
         public ActionResult EditProduct(int id)
         {
             Product p = db.Products.SingleOrDefault(g => g.product_ID == id);
             return View(p);
         }
+
+        [HttpPost]
+        public ActionResult EditProduct(Product model)
+        {
+            ActionResult myAction = RedirectToAction("ProductDetailActionIndex", "ProductDetail");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(model).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                }
+            }
+            catch (Exception a)
+            {
+
+            }
+            return myAction;
+        }
+
+
+     
+        public ActionResult DetailProduct(int id)
+        {
+            Product p = db.Products.SingleOrDefault(g => g.product_ID == id);
+            return View(p);
+        }
+      
+
+
+
     }
 }
